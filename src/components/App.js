33de1ea23ef705069155a199f7252d0b3c145e2e
sachin7con <sachin7con@gmail.com>
2024-12-12 18:26:42 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
+//SGN
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  const handleParaDisplay = () => {
+      setVisible(prev => !prev);
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* ONS  Do not alter the main div */}
+      <button id="click" onClick={handleParaDisplay}>Show Para</button>
+      <p id="para" style={{display: visible ? "block" : "none"}}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      
     </div>
   );
 }
